9b2fa50a7bf02246ca13954a1620fb81373b5ab1
paige <paigevenuto@gmail.com>
2022-07-13 21:26:23 -0400

    will put tsconfig back after fixing rebase

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
         test: /\.css$/,
         use: [{ loader: "style-loader" }, { loader: "css-loader" }],
       },
+      {
+        test: /\.webp$/,
+        use: [{ loader: "webp-loader" }],
+      },
     ],
   },
   plugins: [
